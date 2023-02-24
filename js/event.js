9de8c31ff9f9c66ac808efbfda2f67224bceebed
Enrico0722 <enricoarcilla01@gmail.com>
2023-02-24 17:25:27 +0800

    Added files

--- js/event.js
+++ js/event.js
@@ -2,12 +2,24 @@ window.onresize = function() {
      alert("The window has been resized!");  
     }
 
+for (var i = 0; i < 10; i++){
+        calculateGrade();
+        resetForm();
+}
+function resetForm(){
+        document.getElementById("srForm").reset();
+        document.getElementById("Name").value="";
+        document.getElementById("Courses").value="";
+        document.getElementById("Subjects").value="";
+        document.getElementById("result").value="";
+        document.getElementById("EquivalentGrades").value="";
+        document.getElementById("Graderemarks").value="";
+}
+
 function calculateGrade(){
     //getting the record and grades.
-  
-    window.onresize = function() {
-     alert("The window has been resized!");  
-    }
+          
+
 
     var name = document.getElementById("Names").value;
     var subject = document.getElementById("Subject").value;
@@ -15,50 +27,71 @@ function calculateGrade(){
     var prelim = parseInt(document.getElementById("Prelim").value);
     var midterm = parseInt(document.getElementById("Midterm").value);
     var final = parseInt(document.getElementById("Final").value);
-    
 
-      if(!name=="" && prelim>0 && midterm>0 && final>0)
+
+      if(!name=="" && !course=="" && !subject=="" && prelim>0 && midterm>0 && final>0)
         {
-    
-    
+        
+                   
     var total = prelim + midterm + final;
     var grade = (total / 3).toFixed(2);
+       
 
    
-
     var equivalentGrade;
     var Remarks;
 
     switch (true){
-        case (grade >= 95):
+     case (prelim >= 100 || prelim <= 70  ):
+     case (midterm >= 100 || midterm <= 70 ):
+     case (final >= 100 || final <= 70 ):
+          name = ""
+          course = ""
+          subject = ""
+          grade = ""
+          equivalentGrade = ""
+          Remarks = ""
+          alert ("Please Input a Valid Grade ! ( Grade must be lower than 100 and greater than 70 ) " );
+     break;
+     case (grade >= 100 || grade <= 70):
+             name = ""
+             course = ""
+             subject = ""
+             grade = ""
+             equivalentGrade = ""
+             Remarks = ""
+             alert ("Please Input a Valid Grade !( Grade must be lower than 100 and greater than 70 )");
+     break;
+     case (grade >= 95):
              equivalentGrade = "1.0"
-             Remarks = "Excellent !"
-             break;
-        case (grade >= 90):
+             Remarks = "PASSED!"
+     break;
+     case (grade >= 90):
              equivalentGrade = "1.25"
-             Remarks = "Very Good!"
-             break;    
-        case (grade >= 85):
+             Remarks = "PASSED!"
+     break;    
+     case (grade >= 85):
               equivalentGrade = "1.50"
-              Remarks = "Good!"
-              break;
-        case (grade >= 80):
+              Remarks = "PASSED!"
+     break;
+     case (grade >= 80):
               equivalentGrade = "1.75"
-              Remarks = "Fairly Good!"
-              break;
-        case (grade >= 75):
+              Remarks = "PASSED!"
+     break;
+     case (grade >= 75):
               equivalentGrade = "2.0"
-              Remarks = "Passing!"
-              break;
-         case (grade <= 75):
+              Remarks = "PASSED!"
+     break;
+     case (grade <= 75):
               equivalentGrade = "5.0"
-              Remarks = "Failing!"
-              break;           
+              Remarks = "FAILED!"
+     break; 
+    
     }
      //getting the Result.
 
      const studname = document.getElementById("Name");
-     studname.innerHTML = ` ${name}`;
+     studname.textContent = ` ${name}`;
      
      const studCourse = document.getElementById("Courses");
      studCourse.innerHTML = ` ${course}`;
@@ -70,16 +103,20 @@ function calculateGrade(){
      resultDiv.innerHTML = ` ${grade}`;
 
      var equivalentGradeAve = 
-     document.getElementById("Equivalent Grades");
+     document.getElementById("EquivalentGrades");
      equivalentGradeAve.innerHTML = 
     ` ${equivalentGrade}`;
 
      var gradeRemarks = document.getElementById("Graderemarks");
      gradeRemarks.innerHTML = ` ${Remarks}`;
-  }
+                           
+
+ }
   else{
      alert("Please fill up all the input fields!");
+     
   }
+  
  
 }
 
